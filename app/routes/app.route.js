2cce690574b8550969dee4860c836637be68e0bc
Dien Tran <10137854+dientn@users.noreply.github.com>
2020-06-17 11:25:22 +0700

    Delete app.route.js

--- app/routes/app.route.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-// Load Module dependencies.
-let app = require('../controllers/app');
-
-module.exports = ( router, policies ) => {
-	router.route('/devices').all( policies.isAllowed ).get( app.getAvailables );
-};
